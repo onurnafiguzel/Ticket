b33fd72ed3e4a887de00d00788fe9b7b4b4ea125
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-03-31 21:54:04 +0300

    IEntityRepository oluşturuldu.

---
+++ Ticket.Application/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Abstract;
+
+namespace Ticket.Application.DataAccess
+{
+    internal interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
