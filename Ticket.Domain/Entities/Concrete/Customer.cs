056a9314ca1d4114018e82673bb14846f7cc1758
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-03-31 20:28:07 +0300

    Entities

---
+++ Ticket.Domain/Entities/Concrete/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Abstract;
+
+namespace Ticket.Domain.Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Ticket.Domain/Entities/Concrete/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Abstract;
+
+namespace Ticket.Domain.Entities.Concrete
+{
+    public class Film : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+
+        public decimal Rating { get; set; }
+        public string Director { get; set; }
+    }
+}
