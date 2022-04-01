a89e8bf6e394654c45233161fc4de93492b4fb07
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-04-01 10:17:20 +0300

    Result dönüş tipleri belirlendi.

---
+++ Ticket.Application/Utilities/Results/IDataResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Abstract;
+
+namespace Ticket.Application.Utilities.Results
+{
+    public interface IDataResult<T> : IResult
+    {
+        T Data { get; }
+    }
+}
