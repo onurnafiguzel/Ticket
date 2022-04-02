6ba768ba6847c67b19e7deca7f5707797ec5f7f4
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-04-03 01:01:12 +0300

    AdminValidator eklendi.

---
+++ Ticket.Business/ValidationRules/FluentValidation/AdminValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Business.ValidationRules.FluentValidation
+{
+    public class AdminValidator : AbstractValidator<Admin>
+    {
+        public AdminValidator()
+        {
+            RuleFor(a => a.Name).NotEmpty().WithMessage("İsim alanı boş bırakılamaz.");
+            RuleFor(a => a.Email).EmailAddress().WithMessage("Email adresi boş bırakılamaz.");
+        }
+    }
+}
