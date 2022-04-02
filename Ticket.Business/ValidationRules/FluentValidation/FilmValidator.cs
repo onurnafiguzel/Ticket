d5ec371367b55694b94ed93dfe7878ab690fba50
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-04-03 01:53:57 +0300

    FilmValidator tanımlandı.

---
+++ Ticket.Business/ValidationRules/FluentValidation/FilmValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Business.ValidationRules.FluentValidation
+{
+    public class FilmValidator : AbstractValidator<Film>
+    {
+        public FilmValidator()
+        {
+            RuleFor(f => f.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez.");
+            RuleFor(f => f.Director).NotEmpty().WithMessage("Yönetmen boş geçilemez.");
+            RuleFor(f => f.Rating).NotEmpty().WithMessage("Rating boş geçilemez.");
+            RuleFor(f => f.Duration).NotEmpty().WithMessage("Süre boş geçilemez.");
+            RuleFor(f => f.Description).NotEmpty().WithMessage("Açıklama boş geçilemez.");
+        }
+    }
+}
