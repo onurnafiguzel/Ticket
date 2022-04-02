83c1a1657e559a541efe368b2f98bc66e258e79e
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-04-03 01:41:06 +0300

    CustomerValidator eklendi.

---
+++ Ticket.Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Business.ValidationRules.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez");
+            RuleFor(c => c.Email).EmailAddress().WithMessage("Düzgün bir email girilmelidir.");
+        }
+    }
+}
