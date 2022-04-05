9b36d9abd5ae162c06facf3a862601881952ef88
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-04-05 23:00:04 +0300

    IAuthService ve AuthManager oluşturuldu.

---
+++ Ticket.Business/Abstract/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.Entities.Concrete;
+using Ticket.Application.Utilities.Results;
+using Ticket.Application.Utilities.Security.JWT;
+using Ticket.Domain.Dtos;
+
+namespace Ticket.Business.Abstract
+{
+    public interface IAuthService
+    {
+        Task<IDataResult<Customer>> Register(CustomerForRegisterDto customerForRegisterDto, string password);
+        Task<IDataResult<Customer>> Login(CustomerForLoginDto customerForLoginDto);
+        Task<IResult> UserExists(string email);
+        Task<IDataResult<AccessToken>> CreateAccessToken(Customer customer);
+    }
+}
---
+++ Ticket.Business/Concrete/AuthManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.Entities.Concrete;
+using Ticket.Application.Utilities.Results;
+using Ticket.Application.Utilities.Security.JWT;
+using Ticket.Business.Abstract;
+using Ticket.Domain.Dtos;
+
+namespace Ticket.Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private readonly ICustomerService _customerService;
+        private ITokenHelper _tokenHelper;
+
+        public AuthManager(ITokenHelper tokenHelper, ICustomerService customerService)
+        {
+            _tokenHelper = tokenHelper;
+            _customerService = customerService;
+        }
+
+        public Task<IDataResult<AccessToken>> CreateAccessToken(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<Customer>> Login(CustomerForLoginDto customerForLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<Customer>> Register(CustomerForRegisterDto customerForRegisterDto, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
