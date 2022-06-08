2650dd4973b83dd80af21475a24e51894a577dc4
Enes <enessolak99@gmail.com>
2022-06-09 00:52:54 +0300

    add TicketRepository

---
+++ Ticket.Data/Abstract/ITicketRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.DataAccess;
+
+namespace Ticket.Data.Abstract
+{
+    public interface ITicketRepository : IEntityRepository<Domain.Entities.Concrete.Ticket>
+    {
+    }
+}
---
+++ Ticket.Data/Concrete/EntityFramework/EfTicketRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.DataAccess.EntityFramework;
+using Ticket.Data.Abstract;
+
+namespace Ticket.Data.Concrete.EntityFramework
+{
+    public class EfTicketRepository : EfEntityRepositoryBase<Domain.Entities.Concrete.Ticket>, ITicketRepository
+    {
+        private TicketContext context;
+        public EfTicketRepository(DbContext context) : base(context)
+        {
+            this.context = (TicketContext)context;
+        }
+    }
+}
