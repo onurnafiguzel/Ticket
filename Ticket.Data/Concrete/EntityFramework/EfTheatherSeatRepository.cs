1c22a22441e12c50b7ad2e111088adda645f0cc4
Enes <enessolak99@gmail.com>
2022-06-09 00:13:25 +0300

    add TheatherSeatRepository

---
+++ Ticket.Data/Concrete/EntityFramework/EfTheatherSeatRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.DataAccess.EntityFramework;
+using Ticket.Data.Abstract;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Data.Concrete.EntityFramework
+{
+    public class EfTheatherSeatRepository : EfEntityRepositoryBase<TheatherSeat>, ITheatherSeatRepository
+    {
+        private TicketContext context;
+        public EfTheatherSeatRepository(DbContext context) : base(context)
+        {
+            this.context = (TicketContext)context;
+        }
+    }
+}
