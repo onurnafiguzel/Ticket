efd0aab2968646262838fe471b2cd08fafc5c7de
Enes <enessolak99@gmail.com>
2022-06-09 00:13:45 +0300

    fix TheatherPrice entity

--- Ticket.Domain/Entities/Concrete/TheatherPrice.cs
+++ Ticket.Domain/Entities/Concrete/TheatherPrice.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ticket.Domain.Entities.Abstract;
 using Ticket.Domain.Entities.Enums;
 
 namespace Ticket.Domain.Entities.Concrete
 {
-    public class TheatherPrice
+    public class TheatherPrice : IEntity
     {
         public int Id { get; set; }
         public int TheatherId { get; set; }
