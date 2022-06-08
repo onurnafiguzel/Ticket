a9d11307c49ea3f5bf952a516eeb4a73bf839618
Enes <enessolak99@gmail.com>
2022-06-09 00:13:14 +0300

    add TheatherPriceRepository

---
+++ Ticket.Data/Concrete/EntityFramework/EfTheatherPriceRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.DataAccess.EntityFramework;
+using Ticket.Data.Abstract;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Data.Concrete.EntityFramework
+{
+    public class EfTheatherPriceRepository : EfEntityRepositoryBase<TheatherPrice>, ITheatherPriceRepository
+    {
+        private TicketContext context;
+        public EfTheatherPriceRepository(DbContext context) : base(context)
+        {
+            this.context = (TicketContext)context;
+        }
+    }
+}
