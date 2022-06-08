d280e7d8e9494772ebcf9bbbf61f4b9ed108e800
Enes <enessolak99@gmail.com>
2022-06-09 00:14:00 +0300

    add MovieSessionSeatRepository

---
+++ Ticket.Data/Concrete/EntityFramework/EfMovieSessionSeatRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticket.Application.DataAccess.EntityFramework;
+using Ticket.Data.Abstract;
+using Ticket.Domain.Entities.Concrete;
+
+namespace Ticket.Data.Concrete.EntityFramework
+{
+    public class EfMovieSessionSeatRepository : EfEntityRepositoryBase<MovieSessionSeat>, IMovieSessionSeatRepository
+    {
+        private TicketContext context;
+        public EfMovieSessionSeatRepository(DbContext context) : base(context)
+        {
+            this.context = (TicketContext)context;
+        }
+    }
+}
