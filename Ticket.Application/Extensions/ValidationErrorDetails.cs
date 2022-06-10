a1ff93edd60314b6fd4754c5497245185c1ad779
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-06-10 20:48:25 +0300

    ExceptionMiddleware hazırlandı.

---
+++ Ticket.Application/Extensions/ValidationErrorDetails.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation.Results;
+
+namespace Ticket.Application.Extensions
+{
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
+    }
+}
--- Ticket.WebApi/Program.cs
+++ Ticket.WebApi/Program.cs
@@ -68,6 +68,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.ConfigureCustomExceptionMiddleware();
+
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
