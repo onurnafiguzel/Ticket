0d2fbc8c98c3cd0b4027dc0f30c11c03ebfd4c77
Enes <enessolak99@gmail.com>
2022-06-16 02:26:13 +0300

    migration Actor slug

---
+++ Ticket.Data/Migrations/20220615232111_addSlugToActor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticket.Data.Migrations
+{
+    public partial class addSlugToActor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Actors",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Actors");
+        }
+    }
+}
