8780f40f425f63e6087ce757e79320804357f124
Enes <enessolak99@gmail.com>
2022-06-22 20:46:10 +0300

    Update PlaceDto.cs

--- Ticket.Domain/Dtos/PlaceDto.cs
+++ Ticket.Domain/Dtos/PlaceDto.cs
@@ -11,5 +11,8 @@ namespace Ticket.Domain.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public int CityId { get; set; }
+        public string Address { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
