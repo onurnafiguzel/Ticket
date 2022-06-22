37bc02def88ea6efc206ab80229294394f31fa9e
Enes <enessolak99@gmail.com>
2022-06-22 19:45:13 +0300

    migration for place entity

---
+++ Ticket.Data/Migrations/20220622164219_addNewFieldsToPlaceEntity.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticket.Data.Migrations
+{
+    public partial class addNewFieldsToPlaceEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Places",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "Places",
+                type: "decimal(8,6)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "Places",
+                type: "decimal(9,6)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Places");
+        }
+    }
+}
